717c173247ae47818fb5fce2f9dbb46cbf75ab64
MinhKhoi <mkhoi1776@gmail.com>
2023-02-20 16:40:03 +0700

    Khoi commit

---
+++ diet-calculator/src/java/com/dietcalculator/dto/PopUp.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dietcalculator.dto;
+
+/**
+ *
+ * @author MSI
+ */
+public class PopUp {
+    private String popupID;
+    private String description;
+    private int status;
+    private String theme;
+
+    public PopUp() {
+    }
+
+    public PopUp(String popupID, String description, int status, String theme) {
+        this.popupID = popupID;
+        this.description = description;
+        this.status = status;
+        this.theme = theme;
+    }
+
+    public String getPopupID() {
+        return popupID;
+    }
+
+    public void setPopupID(String popupID) {
+        this.popupID = popupID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+}
---
+++ diet-calculator/src/java/com/dietcalculator/dto/ProductDetail.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dietcalculator.dto;
+
+/**
+ *
+ * @author MSI
+ */
+public class ProductDetail {
+    private String detailID;
+    private String saleoffID;
+    private String productID;
+    private int percentoff;
+    private float priceoff;
+    private String gift;
+
+    public ProductDetail() {
+    }
+
+    public ProductDetail(String detailID, String saleoffID, String productID, int percentoff, float priceoff, String gift) {
+        this.detailID = detailID;
+        this.saleoffID = saleoffID;
+        this.productID = productID;
+        this.percentoff = percentoff;
+        this.priceoff = priceoff;
+        this.gift = gift;
+    }
+
+    public String getDetailID() {
+        return detailID;
+    }
+
+    public void setDetailID(String detailID) {
+        this.detailID = detailID;
+    }
+
+    public String getSaleoffID() {
+        return saleoffID;
+    }
+
+    public void setSaleoffID(String saleoffID) {
+        this.saleoffID = saleoffID;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public int getPercentoff() {
+        return percentoff;
+    }
+
+    public void setPercentoff(int percentoff) {
+        this.percentoff = percentoff;
+    }
+
+    public float getPriceoff() {
+        return priceoff;
+    }
+
+    public void setPriceoff(float priceoff) {
+        this.priceoff = priceoff;
+    }
+
+    public String getGift() {
+        return gift;
+    }
+
+    public void setGift(String gift) {
+        this.gift = gift;
+    }
+    
+    
+}
