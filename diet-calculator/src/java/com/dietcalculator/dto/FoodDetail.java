c036b173d39b425ae626b16708f480b56dbb0b02
PhiNSE <nguyenphigv23@gmail.com>
2023-02-26 09:59:30 +0700

    phii

--- diet-calculator/src/java/com/dietcalculator/dto/FoodDetail.java
+++ diet-calculator/src/java/com/dietcalculator/dto/FoodDetail.java
@@ -8,26 +8,15 @@ public class FoodDetail {
     private String foodID;
     private String mealID;
     private double amount;
-    double totalCal;
-    double carbohydrate;
-    double fiber;
-    double protein;
-    double fat;
-    double water;
     
     public FoodDetail() {
     }
 
-    public FoodDetail(String foodID, String mealID, double amount, double totalCal, double carbohydrate, double fiber, double protein, double fat, double water) {
+    public FoodDetail(String foodID, String mealID, double amount) {
         this.foodID = foodID;
         this.mealID = mealID;
         this.amount = amount;
-        this.totalCal = totalCal;
-        this.carbohydrate = carbohydrate;
-        this.fiber = fiber;
-        this.protein = protein;
-        this.fat = fat;
-        this.water = water;
+
     }
 
     public double getAmount() {
@@ -54,51 +43,4 @@ public class FoodDetail {
         this.mealID = mealID;
     }
 
-    public double getTotalCal() {
-        return totalCal;
-    }
-
-    public void setTotalCal(double totalCal) {
-        this.totalCal = totalCal;
-    }
-
-    public double getCarbohydrate() {
-        return carbohydrate;
-    }
-
-    public void setCarbohydrate(double carbohydrate) {
-        this.carbohydrate = carbohydrate;
-    }
-
-    public double getFiber() {
-        return fiber;
-    }
-
-    public void setFiber(double fiber) {
-        this.fiber = fiber;
-    }
-
-    public double getProtein() {
-        return protein;
-    }
-
-    public void setProtein(double protein) {
-        this.protein = protein;
-    }
-
-    public double getFat() {
-        return fat;
-    }
-
-    public void setFat(double fat) {
-        this.fat = fat;
-    }
-
-    public double getWater() {
-        return water;
-    }
-
-    public void setWater(double water) {
-        this.water = water;
-    }
 }
