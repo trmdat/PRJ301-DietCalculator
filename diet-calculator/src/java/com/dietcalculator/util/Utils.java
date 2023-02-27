b587381242a1b3dfd80db30a4f226aa00cf66232
PhiNSE <nguyenphigv23@gmail.com>
2023-02-27 08:17:08 +0700

    phi

--- diet-calculator/src/java/com/dietcalculator/util/Utils.java
+++ diet-calculator/src/java/com/dietcalculator/util/Utils.java
@@ -10,14 +10,25 @@ package com.dietcalculator.util;
  * @author asout
  */
 public class Utils {
-    public static int extractIntFromString(String input){
+
+    private Utils() {
+
+    }
+
+    public static int extractIntFromString(String input) {
         int i = 0;
-        while(Character.isDigit(input.charAt(i)))
+        while (Character.isDigit(input.charAt(i))) {
             i++;
+        }
         return Integer.valueOf(input.substring(i));
     }
-    
-    public static int randomInt(int lower, int upper){
+
+    public static int randomInt(int lower, int upper) {
         return (int) (lower + (upper - lower + 1) * Math.random());
+    }
+
+    public static java.sql.Date convertJavaDateToSqlDate(int year, int month, int day) {
+        java.util.Date utilDate = new java.util.Date(year - 1900, month, day);
+        return new java.sql.Date(utilDate.getTime());
     }
 }
